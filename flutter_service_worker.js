fe0f55020eacd4ca0811691485358393042c8e7b
Omkar Sutar <omkarsutar37@gmail.com>
2026-01-19 22:36:22 +0530

    change

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,6 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"assets/AssetManifest.bin": "2ce5b594aa15238f434db6934ce0f72c",
 "assets/AssetManifest.bin.json": "663c7d8baf46de25410e4e44acb5f3ff",
-"assets/AssetManifest.json": "79d02f2f519ac84b2c2eb4db62926e0a",
 "assets/assets/fonts/Roboto-Bold.ttf": "dd5415b95e675853c6ccdceba7324ce7",
 "assets/assets/fonts/Roboto-Regular.ttf": "303c6d9e16168364d3bc5b7f766cfff4",
 "assets/assets/images/rasoiaImageUsedOnBill.png": "606ffc1904fa5513f2089fb477575f79",
@@ -40,14 +39,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "2ce5b594aa15238f434db6934ce0f72c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "f441d476a41150d5048f0fe123b43963",
+"flutter_bootstrap.js": "5cd370eb73332587602f12154e1a9dc8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "fa394b12cf0431d0b57abe0f01feff2a",
 "/": "fa394b12cf0431d0b57abe0f01feff2a",
-"main.dart.js": "7a43d7117cad939f09b0d59e662ce027",
+"main.dart.js": "68ff3bad174ad461a8aa921fbdd5f865",
 "manifest.json": "07eabd3fcc1a9f3a6f1a96fdd0fb32b0",
 "version.json": "6279b240ff3d46770d2459f7399c12ce"};
 // The application shell files that are downloaded before a service worker can
