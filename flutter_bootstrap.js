37324cb51a62b370e40f10a0c44c50264996acdf
Omkar Sutar <omkarsutar37@gmail.com>
2026-01-30 16:17:58 +0530

    after SRP start

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "256136642"
+    serviceWorkerVersion: "3045089143"
   }
 });
